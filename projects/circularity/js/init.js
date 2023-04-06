844364a924310d2d8789163bc2e2aba5150733f4
ntierney25 <ntierney25@bfhsla.org>
2023-04-06 00:08:32 +0000

    Used Array and thus was able to finish circularity!

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -34,11 +34,9 @@ var init = function (window) {
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
+        for (var i = 0; i<100; i++){
+            drawCircle();
+        }
 
 
 
@@ -53,21 +51,14 @@ var init = function (window) {
         */
         function update() {
 
-            physikz.updatePosition(circles[0]);
-	        physikz.updatePosition(circles[1]);
-	        physikz.updatePosition(circles[2]);
-	        physikz.updatePosition(circles[3]);
-	        physikz.updatePosition(circles[4]);
-
-            
-            // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           game.checkCirclePosition(circles[0]);
-           game.checkCirclePosition(circles[1]);
-           game.checkCirclePosition(circles[2]);
-           game.checkCirclePosition(circles[3]);
-           game.checkCirclePosition(circles[4]);
 
             // TODO 9 : Iterate over the array
+
+            for (var i=0; i< circles.length; i++){
+                var eachCircle = circles[i];
+                physikz.updatePosition(eachCircle);
+                game.checkCirclePosition(eachCircle);
+            }
 
               
         }
@@ -85,7 +76,17 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if (circle.x < 0){
+                circle.x = canvas.width
+            }
+
+            if (circle.y > canvas.height){
+                circle.y = 0
+            }
+
+            if (circle.y < 0){
+                circle.y = canvas.height
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
